5188e6a77a363c3efb3ba675c0153fb52647185d
vizmoe <hi@viz.moe>
2026-07-09 06:18:24 -0400

    Fix cask audit style

--- Casks/s/subrenamer.rb
+++ Casks/s/subrenamer.rb
@@ -5,8 +5,7 @@ cask "subrenamer" do
   sha256 arm:   "8876349f691ad149ed03f4adb9bacebbda9695671e984c9474b0887063c7cb72",
          intel: "1a05ce5a952e23bf536b2a054ee310d03243f2d48924a1edc1300ae78a64564f"
 
-  url "https://github.com/qwqcode/SubRenamer/releases/download/v#{version}/SubRenamer_macos_#{arch}.zip",
-      verified: "github.com/qwqcode/SubRenamer/"
+  url "https://github.com/qwqcode/SubRenamer/releases/download/v#{version}/SubRenamer_macos_#{arch}.zip"
   name "SubRenamer"
   desc "Batch rename subtitle files to match video names"
   homepage "https://github.com/qwqcode/SubRenamer"
@@ -15,6 +14,8 @@ cask "subrenamer" do
     url :url
     strategy :github_latest
   end
+
+  depends_on :macos
 
   app "SubRenamer.app"
 
